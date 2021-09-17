dc30f84812bc80208325488dfbac6006cf3435e9
Akbar Agha <akbar.agha@grainger.com>
2021-09-17 13:26:38 -0500

    first commit

---
+++ input.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# traffic_citations = pd.read_csv('https://s3-us-west-2.amazonaws.com/pcadsassessment/parking_citations.corrupted.csv')
+traffic_citations = pd.read_csv('https://s3-us-west-2.amazonaws.com/pcadsassessment/parking_citations.corrupted.csv')
+print(traffic_citations)
